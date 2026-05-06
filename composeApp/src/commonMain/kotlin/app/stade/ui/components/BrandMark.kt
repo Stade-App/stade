f538352926b91c25e596e5f8deabcf5074add65d
nyaexx <70057364+nyaexx@users.noreply.github.com>
2026-05-06 20:58:21 +0300

    refactor: clean up imports in BrandMark.kt

--- composeApp/src/commonMain/kotlin/app/stade/ui/components/BrandMark.kt
+++ composeApp/src/commonMain/kotlin/app/stade/ui/components/BrandMark.kt
@@ -1,22 +1,14 @@
 package app.stade.ui.components
 
-import androidx.compose.foundation.Image // Bunu ekle
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource // KMP'nin resource kütüphanesi
 import stade.composeapp.generated.resources.Res
-import stade.composeapp.generated.resources.ic_launcher_foreground
 import stade.composeapp.generated.resources.ic_launcher_monochrome
 
 
