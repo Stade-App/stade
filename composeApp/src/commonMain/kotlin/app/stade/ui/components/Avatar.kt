3cb1bfc3334969b0b2c825e96cb224e75eef893e
nyaexx <70057364+nyaexx@users.noreply.github.com>
2026-05-06 21:13:31 +0300

    refactor: hatalı uygulama logosu dosya dizimi düzeltildi

--- composeApp/src/commonMain/kotlin/app/stade/ui/components/Avatar.kt
+++ composeApp/src/commonMain/kotlin/app/stade/ui/components/Avatar.kt
@@ -3,10 +3,8 @@ package app.stade.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
