634721760135d6c67fbe10f043da3d7070b26b6d
noydra <noydra@users.noreply.github.com>
2026-08-04 17:18:17 +0300

    Bump versionCode to 10 for Play Console re-upload

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -98,7 +98,7 @@ android {
         applicationId = "dev.stade"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "0.1.8"
     }
 
